3c58c146709b84ec1022b128ac021bc6a29590f3
Макар <120005351+MakarSux@users.noreply.github.com>
2024-08-07 21:12:56 +0300

    Add files via upload

--- js/dev_swiper.js
+++ js/dev_swiper.js
@@ -3,59 +3,75 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
 
-        // const swiper = new Swiper('.swiper', {
-        //     speed: 400,
-        //     spaceBetween: 100,
-        // });
-        // const swiper = document.querySelector('.swiper').swiper
-        // // Now you can use all slider methods like
-        // swiper.slideNext();
-
         const swiper = new Swiper('.swiper-test', {
             // Optional parameters
             slidesPerView: 3,
             spaceBetween: 70,
 
             centeredSlides: true,
-            centeredSlidesBounds: true,
+            centeredSlidesBounds: false,
 
+            breakpoints: {
+                270: {
+                    centeredSlides: true,
+                    slidesPerView: 1,
+                    spaceBetween: 10
+                },
+                415: {
+                    centeredSlides: true,
+                    slidesPerView: 2,
+                    spaceBetween: 10
+                },
+                480: {
+                    centeredSlides: true,
+                    slidesPerView: 2,
+                    spaceBetween: 10
+                },
+                570: {
+                    centeredSlides: true,
+                    slidesPerView: 2,
+                    spaceBetween: 10
+                },
+                1000: {
+                    centeredSlides: true,
+                    slidesPerView: 3,
+                    spaceBetween: 37,
+                },
+                1170: {
+                    centeredSlides: true,
+                    slidesPerView: 3,
+                    spaceBetween: 40,
+                },
+                1350: {
+                    slidesPerView: 3,
+                    spaceBetween: 10,
+                },
+                1590: {
+                    centeredSlides: true,
+                    centeredSlidesBounds: false,
+                    slidesPerView: 3,
+                    spaceBetween: 70,
+                },
+                1700: {
+                    slidesPerView: 3,
+                    spaceBetween: 70,
 
-            // breakpoints: {
-            //     // when window width is >= 320px
-            //     320: {
-            //       slidesPerView: 2,
-            //       spaceBetween: 20
-            //     },
-            //     // when window width is >= 480px
-            //     480: {
-            //       slidesPerView: 3,
-            //       spaceBetween: 30
-            //     },
-            //     // when window width is >= 640px
-            //     640: {
-            //       slidesPerView: 4,
-            //       spaceBetween: 40
-            //     }
-            //   }
+                    centeredSlides: true,
+                    centeredSlidesBounds: true,
+                }
+            },
 
             direction: 'horizontal',
             loop: true,
 
-            // If we need pagination
             pagination: {
                 el: '.swiper-pagination',
             },
 
-            // Navigation arrows
             navigation: {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             },
-
-            // And if we need scrollbar
-            // scrollbar: {
-            //     el: '.swiper-scrollbar',
-            // },
         });
 
     }, 0);
