41e8fbecc0034d0249c89a2dac26211073fb7d12
Макар <120005351+MakarSux@users.noreply.github.com>
2024-08-12 19:46:00 +0300

    Add files via upload

---
+++ js/closeQRCode.js
@@ -0,0 +1,5 @@
+let QRCodeX = document.querySelector('.qr-code__x')
+QRCodeX.addEventListener('click', () => {
+    let QRCode = document.querySelector('.qr-code')
+    QRCode.classList.add('hidden')
+})
